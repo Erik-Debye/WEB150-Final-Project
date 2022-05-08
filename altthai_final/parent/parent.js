03ec8490095b1c9e523b3b8b13d185acf5b68c17
Erik Debye <erik.debye@gmail.com>
2022-05-07 23:05:05 -0500

    Major changes to the parent.js
    Changes to behavior upon browser resizing when tablet is switched between layouts

--- altthai_final/parent/parent.js
+++ altthai_final/parent/parent.js
@@ -20,3 +20,24 @@ hamburgerIcon.addEventListener('click', (e) => {
   cta.classList.toggle('show');
   hamNav.classList.toggle('show');
 });
+
+//set a JS media query
+//Checks if window size is resized to +768px && if hamburger menu is open.
+//if open, it will remove show class and auto close the hamburger menu in favor for the normal menu (which is reactivated in a CSS media query)
+//This is done in JS because seeked bahavior is depandent on a conditional (if window size is above 768px)
+const mediaQuery = window.matchMedia('(min-width: 769px)');
+
+//set event listener to listen for size check
+mediaQuery.addEventListener('change', resizeCheck);
+
+function resizeCheck(e) {
+  if (e.matches && hamNav.classList.contains('show')) {
+    e.preventDefault();
+    hamLine1.classList.toggle('change--1');
+    hamLine2.classList.toggle('change--2');
+    hamLine3.classList.toggle('change--3');
+    hamNav.classList.toggle('show');
+  }
+}
+
+resizeCheck(mediaQuery);
